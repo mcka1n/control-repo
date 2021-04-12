654d05e344122c3a7ac1e4d723817801ace90cc5
Ed Mckain <mckain.default@gmail.com>
2021-04-12 11:17:05 +0200

    enforces VIA as group

--- site-modules/profile/manifests/platform/baseline/users/windows.pp
+++ site-modules/profile/manifests/platform/baseline/users/windows.pp
@@ -1,4 +1,8 @@
 class profile::platform::baseline::users::windows {
+  group { 'Vandelay Industries Administrators':
+    ensure => present,
+  }
+
   user { 'Art Vandelay':
     ensure   => present,
     password => 'Ha!!ochenTest0',
