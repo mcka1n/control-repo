18960b601837a31b07ff3e0e1214cc643148a539
Edwin Maldonado <mckain.default@gmail.com>
2021-04-29 12:10:18 +0200

    IIS Webserver role (#2)
    * adds the puppetlabs-iis module
    
    * adds profiles to the webserver role
    
    * adds functionality to the iis_server profile
    
    * enforces basic website for IIS
    
    * adds puppetlabs-reboot
    
    * adds class block for choco debugging purposes
    
    * uses a file resource to download the html file
    
    * sets permissions for the IIS website folder

---
+++ site-modules/profile/manifests/platform/windows/webserver/iis_server.pp
@@ -0,0 +1,49 @@
+class profile::platform::windows::webserver::iis_server {
+  # Minimal features necessary to ensure an IIS installation
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+  $iss_folders_group = 'IIS_IUSRS'
+
+  iis_feature { $iis_features:
+    ensure => 'present',
+  }
+
+  # Delete the default website to prevent a port binding conflict.
+  iis_site {'Default Web Site':
+    ensure  => absent,
+    require => Iis_feature['Web-WebServer'],
+  }
+
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'DefaultAppPool',
+    require         => [
+      File['minimal'],
+      Iis_site['Default Web Site']
+    ],
+  }
+
+  file { 'minimal':
+    ensure => 'directory',
+    path   => 'c:\\inetpub\\minimal',
+  }
+
+  # Set Permissions
+  acl { 'c:\\inetpub\\minimal':
+    permissions => [
+      { identity => $iss_folders_group, rights => ['read', 'execute']},
+    ],
+  }
+
+  # Adds basic website
+  $install_path = 'C:/inetpub/minimal'
+  $html_file_source = 'https://gist.githubusercontent.com/dylanratcliffe/af0e24303d241b888152bd1cd7c9063d/raw/ad273bebc01c6dac176da7a5f3c38c4d9a584521/index.html'
+
+  file { "${install_path}/index.html":
+    path        => "${install_path}/index.html",
+    ensure      => 'present',
+    source      => $html_file_source,
+    group       => $iss_folders_group,
+    mode        => '0555',
+  }
+}
