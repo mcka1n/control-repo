b6b6a66ad26f1f876628e46e27847250917554a8
Ed Mckain <mckain.default@gmail.com>
2021-10-22 16:12:59 +0200

    sets the test scenario for PE testing

--- site-modules/solarch/plans/load_test_plan.pp
+++ site-modules/solarch/plans/load_test_plan.pp
@@ -2,10 +2,10 @@ plan solarch::load_test_plan (
   String $message,
 ) {
   out::message("Running solarch::load_test_plan, message: ${message}")
-  out::message('|_ 1st 30 sec sleep')
-  ctrl::sleep(30)
-  out::message('|_ 2nd 30 sec sleep')
-  ctrl::sleep(30)
-  out::message('|_ 3rd 30 sec sleep')
-  ctrl::sleep(30)
+  out::message('|_ 1st 7 min sleep')
+  ctrl::sleep(420)
+  out::message('|_ 2nd 7 min sleep')
+  ctrl::sleep(420)
+  out::message('|_ 3rd 7 min sleep')
+  ctrl::sleep(420)
 }
